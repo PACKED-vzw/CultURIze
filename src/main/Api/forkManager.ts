63210f0fa461fe00f1fa89fc4302b22866aa4e88
Bert Schoovaerts <bert.schoovaerts@student.kuleven.be>
2018-07-17 11:35:42 +0200

    forkmanager working TODO handle statuscodes

--- src/main/Api/forkManager.ts
+++ src/main/Api/forkManager.ts
@@ -1,3 +1,5 @@
+import { resolve } from "dns";
+
 const octokit = require('@octokit/rest')()
 
 export class ForkManager {
@@ -7,12 +9,19 @@ export class ForkManager {
     }
     repoURL:string
 
-    public forkRepo():string{
-        var result = ""
-        //parse URL here
-        this.createFork("dummy", "dummy"," token")
-
-        return result
+    public async forkRepo():Promise<string>{
+        try{
+            var result = ""
+            //parse URL here
+            const fork_url:string = await this.createFork("dummy", "dummy","token")
+            await this.updateFork("dummy", "dummy", "token")
+    
+            return result
+        }
+        catch(error)
+        {
+            console.error(error)
+        }
     }
     private authenticate(token: string) :void {
         octokit.authenticate({
@@ -20,46 +29,86 @@ export class ForkManager {
             token: token
         })
     }
-    private createFork(owner: string, repo: string, token:string){
-        octokit.repos.fork({
+    private createFork(owner: string, repo: string, token:string):Promise<string>{
+        return new Promise<string>((resolve,reject)=> octokit.repos.fork({
             owner : "oSoc18",
             repo  : "resolver"
         }, (error:any, result:any) => {
-            console.log(result)
-            console.log("error: " + error)
-            })
-        }
-        private updateFork(owner:string, repo:string, token:string){
+            if(error != null){
+                console.log("could not create a fork: "+ error)
+                reject(error)
+            }
+            else{
+                console.log(result)
+                console.log("A fork was succesfully created")
+                resolve(result.data.url)
+            }
+         }))
+       
+    }
+    private updateFork(owner:string, repo:string, token:string):Promise<void>{
+        return new Promise<void>((resolve,reject)=> {
             octokit.gitdata.getReference({
                 owner : "oSoc18",
                 repo  : "resolver",
                 ref   : "heads/master"
             },(error:any, result:any) => {
                 //console.log(result.data.object)
-               // console.log(error)
-               
-                const resultobject = result.data.object
-                const sha = resultobject.sha
-                const type = resultobject.type
-                const url = resultobject.url
-                console.log("sha: "+ sha)
-                console.log("type: "+ type)
-                console.log("url: " + url)
-                //this.merge("dummy", "dummy", sha, token)
-                
-        
+                // console.log(error)
+                if(error != null){
+                    console.log("could not get the reference to the upstream head")
+                    console.log(error)
+                    reject(error)
+                }
+                else{
+                    const resultobject = result.data.object
+                    const sha = resultobject.sha
+                    const type = resultobject.type
+                    const url = resultobject.url
+                    console.log("sha: "+ sha)
+                    console.log("type: "+ type)
+                    console.log("url: " + url)
+                    console.log("succesfully got the reference to upstream head")
+                    const statuscode = this.merge("dummy", "dummy", sha, token)
+                    .then((statuscode:number)=>
+                    {
+                        console.log("ksldfjqlsisefjsqiljqlskejqlejfmama")
+                        switch (statuscode)
+                        {
+                            case 202:
+                                console.log("merge accepted")
+                                break;
+                            default: 
+                                console.log(statuscode);
+                        }
+                        resolve()
+                    })
+                    .catch((err:any)=>{
+                        console.log("qlksdjflqsjflmsfpapa")
+                        console.error(error)
+                        reject(error)
+                    })
+                }
             })
-        }
-        private merge(owner:string, repo:string, sha:string, token:string):void{
+        })    
+    }
+    private merge(owner:string, repo:string, sha:string, token:string): Promise<number>{
+        return new Promise<number>((resolve,reject)=> {
             octokit.repos.merge({
                 owner : "BertSchoovaerts",
                 repo  : "resolver",
                 base  : "master",
                 head  : sha,
-                commit_message : "testerdefloepswoeps"
+                commit_message : "nieuwe update test"
             },(error:any, result:any) => {
-                console.log(result)
+                console.log("hakuna mata matafaka")
                 console.log(error)
+                if(error != null)
+                    reject(error)
+                else
+                    resolve(result.status)
+                        
             })
-        }
+        })
+    }
 }

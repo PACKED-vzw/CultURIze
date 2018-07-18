7a9e6928e2242217cd33beb8d2b5ccd395b69918
Bert Schoovaerts <bert.schoovaerts@student.kuleven.be>
2018-07-18 13:24:16 +0200

    User class finished

--- src/main/Api/User.ts
+++ src/main/Api/User.ts
@@ -11,19 +11,26 @@ export class User{
     constructor(userName: string, avatar_url: string){
         this.userName = userName
         this.avatar_url = avatar_url
+
     }
 
     public static getUserInfo(token: string): Promise<User>{
+        octokit.authenticate({
+            type: "oauth",
+            token: token
+        })
         return new Promise<User>((resolve,reject)=> {
-            octokit.users.get({}).then((result: any) =>{
-               const userName:string = result.login
-               const avatar_url:string = result.avatar_url
+            octokit.users.get({})
+            .then((result: any) =>{
+               const userName:string = result.data.login
+               const avatar_url:string = result.data.avatar_url
                var newUser: User = new User(userName, avatar_url)
                resolve(newUser)
             })
             .catch((error: any) => {
-            console.log("error getting UserInfo: ")
-            console.log(error)
+                console.log("error getting UserInfo: ")
+                console.log(error)
+                reject("failed to retrieve user info")
             })
         })
     }

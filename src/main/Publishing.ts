100bd2b0edbd3ffd3cf73a84c22a63f2dfc49537
Pierre-vh <pierre.vanhoutryve@outlook.com>
2018-07-18 14:48:10 +0200

    Work stuff

--- src/main/Publishing.ts
+++ src/main/Publishing.ts
@@ -11,6 +11,7 @@ import { mainWindow } from "./../main";
 import { ForkManager } from "./Api/ForkManager";
 import { convertCSVtoHTACCESS } from "./Converter/Converter";
 import { GitRepoManager } from "./Git";
+import { User } from './Api/User'
 const isGithubUrl = require("is-github-url");
 const octokit = require("@octokit/rest")();
 const GitUrlParse = require("git-url-parse");
@@ -22,30 +23,48 @@ export async function publish(request: PublishRequest) {
         console.log("Checking Request for incorrect input");
         await checkRequestInput(request);
 
+        // Gather user information
+        let user = await User.getUserInfo(request.token)
+
         // Parse the url
         console.log("Parsing URL");
         const parsedURL = GitUrlParse(request.repoUrl);
         const destOwner = parsedURL.owner;
         const destName = parsedURL.name;
         const prettyName = destOwner + "/" + destName;
-        console.log(
-            "Parsed URL: owner: " + destOwner
-            + " name: " + destName
-            + " prettyName: " + prettyName,
-        );
+        console.log({ repoOwner:destOwner, repoName:destName })
 
-        // Convert the file
-        const content = await convertCSVtoHTACCESS(request.csvPath);
+        let isOwnerOfRepo = (destOwner === user.userName)
 
-        // Fork the goal repo if we don't own it
-        console.log("Attempting to fork " + prettyName);
-        const forks = new ForkManager(request.token);
-        const url = await forks.forkRepo(request.repoUrl);
-        console.log("Successfully forked " + prettyName + ' at "' + url + '"');
+        // Convert the file before doing anything with the GitHub api,
+        // so if this steps fail, we don't mess with the GitHub api
+        let content = await convertCSVtoHTACCESS(request.csvPath);
+
+        // The URL of the repo that we're going to clone/manage.
+        let repoURL : string
+
+        if(isOwnerOfRepo)
+        {
+            // If the current user owns the repo, no forking will be required
+            console.log(user.userName + ' owns ' + request.repoUrl + ', no forking required')
+            repoURL = request.repoUrl
+        }
+        else 
+        {
+            // If the current user doesn't own the repo, we'll have to fork it
+
+            // Fork the goal repo if we don't own it
+            console.log("Attempting to fork " + prettyName);
+            const forks = new ForkManager(request.token);
+
+            // Todo: replace master with request.branch when it's added
+            repoURL = await forks.forkRepo(request.repoUrl, user.userName, "master");
+            console.log("Successfully forked " + prettyName + ' at "' + repoURL + '"');
+        }
 
         // Prepare the repoManager
         console.log("Preparing GitRepoManager instance");
-        const manager = await prepareGitRepoManager(url, request.token);
+        const manager = await prepareGitRepoManager(repoURL, request.token);
 
         // Save the file
         console.log("Saving the .htaccess to the desired location");
@@ -55,11 +74,17 @@ export async function publish(request: PublishRequest) {
         console.log("Pushing changes");
         await manager.pushChanges("Culturize import", "master");
 
-        // Make the pull request
-        console.log("Creating pull request");
-        await createPullRequest(request.token, destOwner, destName, "Pierre-vh", "master",
-            () => "some title", () => "some body");
+        // Make the pull request if we don't own the repo
+        if(!isOwnerOfRepo)
+        {
+            console.log("Creating pull request");
+            await createPullRequest(request.token, destOwner, destName, "Pierre-vh", "master",
+                () => "some title", () => "some body");
+        }
+        else 
+            console.log("The current owner owns the repo, no Pull Request required")
 
+        console.log('Done !')
         sendRequestResult(
             new PublishRequestResult(true),
         );
@@ -123,11 +148,20 @@ function checkRequestInput(request: PublishRequest): Promise<void> {
     return new Promise<void>((resolve, reject) => {
         if (!isGithubUrl(request.repoUrl)) {
             reject('"' + request.repoUrl + '" is not a valid GitHub repository');
-        } else if ((request.subdir.length > 0) && (!/^((\w)+)(((\/)(\w+))+)?$/.test(request.subdir))) {
-            console.log("length: " + request.subdir.length);
+            return
+        } 
+        
+        if ((request.subdir.length > 0) && (!/^((\w)+)(((\/)(\w+))+)?$/.test(request.subdir))) {
             reject('"' + request.subdir + '" is not a valid path');
-        } else {
-            resolve();
-             }
+            return
+        } 
+        
+        if((request.token === "") || (request.token == null))
+        {
+            reject("Unauthorized user (empty/null token)")
+            return
+        }
+
+        resolve();
     });
 }

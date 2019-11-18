a86f177fc42e71e48f00b69bebcb56a32018231a
Douwe De Bock <douwe@linuxbe.com>
2019-11-18 16:42:53 +0100

    more cli logging

--- src/cli.ts
+++ src/cli.ts
@@ -6,35 +6,43 @@ import { convertCSVtoWebConfig } from "./main/Converter/Converter";
 import fs = require("fs");
 const log = require("electron-log");
 
-function parseCLIArgs(argv: any) {
-    const args = require("yargs")
-        .option("n", {
-            alias: "nginx",
-            type: "boolean",
-            default: false})
-        .option("a", {
-            alias: "apache",
-            type: "boolean",
-            default: false})
-        // .option("t", {
-        //    alias: "auto-add",
-        //    type: "boolean",
-        //    default: false})
-        .option("csv", {
-            type: "string",
-            nargs: 1,
-            demand: true})
-        .argv;
+function parseCLIArgs(cliargs: any) {
+  log.info(cliargs);
+  log.info("parse");
+  const args = require("yargs")
+      .option("n", {
+          alias: "nginx",
+          type: "boolean",
+          default: false})
+      .option("a", {
+          alias: "apache",
+          type: "boolean",
+          default: false})
+      .option("csv", {
+          type: "string",
+          nargs: 1,
+          demand: true})
+      .parse(cliargs)
+      .argv;
 
-    if (args.nginx === true && args.apache === true) {
-      log.error("choose one: apache or nginx");
-      app.exit(1);
-    }
+      // .option("t", {
+      //    alias: "auto-add",
+      //    type: "boolean",
+      //    default: false})
 
-    return args;
+  log.info("parse done?");
+  log.info(args);
+  if (args.nginx === true && args.apache === true ||
+      args.nginx === false && args.apache === false) {
+    log.error("choose one: apache or nginx");
+    app.exit(1);
+  }
+
+  return args;
 }
 
 export async function mainCLI(argv: any) {
+  log.info("maincli");
   const args = parseCLIArgs(argv);
   const response = await convertCSVtoWebConfig(args.csv, args.apache);
   log.info("Conversion result: " + response.file.length + " characters in the configuration, generated from "
